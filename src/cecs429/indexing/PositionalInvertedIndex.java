0e520aa5f5ed5cd7b85a9f3ba6bf9a21a2dac5f7
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2022-09-13 08:33:33 -0700

    inverted index

---
+++ src/cecs429/indexing/PositionalInvertedIndex.java
@@ -0,0 +1,46 @@
+package cecs429.indexing;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class PositionalInvertedIndex implements Index{
+    private HashMap<String, List<Posting>>  termToDocID = new HashMap<>();
+    @Override
+    public List<Posting> getPostings(String term) {
+        if(termToDocID.containsKey(term)){
+            return termToDocID.get(term);
+        }
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<String> getVocabulary() {
+        List<String> keys = new ArrayList<>(termToDocID.keySet());
+        Collections.sort(keys);
+        return keys;
+    }
+    /**
+     * Associates the given documentId with the given term in the index.
+     */
+    public void addTerm(String term, int documentId, int pos) {
+        if(!termToDocID.containsKey(term)){
+            List<Posting> docIDs = new ArrayList<>();
+            termToDocID.put(term, docIDs);
+            Posting posting = new Posting(documentId);
+            posting.addPos(pos);
+            docIDs.add(posting);
+        } else {
+            List<Posting> docsIDs = termToDocID.get(term);
+            if(docsIDs.get(docsIDs.size() - 1).getDocumentId() != documentId){
+                Posting posting = new Posting(documentId);
+                posting.addPos(pos);
+                docsIDs.add(posting);
+            }else {
+                docsIDs.get(docsIDs.size()-1).addPos(pos);
+            }
+        }
+    }
+}
--- src/cecs429/indexing/Posting.java
+++ src/cecs429/indexing/Posting.java
@@ -1,11 +1,16 @@
 package cecs429.indexing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A Posting encapulates a document ID associated with a search query component.
  */
 public class Posting {
 	private int mDocumentId;
-	
+	private List<Integer> mPos =new ArrayList<>();
+	public void addPos(int pos){mPos.add(pos);}
+
 	public Posting(int documentId) {
 		mDocumentId = documentId;
 	}
