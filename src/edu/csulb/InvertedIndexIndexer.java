57834161518e4966464d68c7a0b60d656d2dbb05
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2022-09-16 17:48:23 -0700

    queries

--- src/edu/csulb/InvertedIndexIndexer.java
+++ src/edu/csulb/InvertedIndexIndexer.java
@@ -6,6 +6,7 @@ import cecs429.indexing.PositionalInvertedIndex;
 import cecs429.indexing.Posting;
 import cecs429.text.BasicTokenProcessor;
 import cecs429.text.EnglishTokenStream;
+import org.tartarus.snowball.SnowballStemmer;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -13,11 +14,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class InvertedIndexIndexer {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         // Create a DocumentCorpus to load .txt documents from the project directory.
-        DocumentCorpus corpus = findCorpus();
+         DirectoryCorpus corpus = findCorpus();
         // Index the documents of the corpus.
         Index index = indexCorpus(corpus);
         InputStreamReader inp = new InputStreamReader(System.in);
@@ -26,10 +28,15 @@ public class InvertedIndexIndexer {
         do {
             System.out.print("Enter query: ");
             userInput = br.readLine();
-            for (Posting p : index.getPostings(userInput)) {
+            String trimmed = userInput.trim();
+            if (trimmed.startsWith(":"))
+            {
+                SpecialQuery(trimmed,corpus,index);
+            }
+            else {for (Posting p : index.getPostings(userInput)) {
                 System.out.println("Document " + corpus.getDocument(p.getDocumentId()).getTitle());
             }
-            System.out.print("\n\n\n");
+            System.out.print("\n\n\n");}
         } while (!userInput.equals("quit"));
     }
 
@@ -48,6 +55,44 @@ public class InvertedIndexIndexer {
             }
         }
         return invertedIndex;
+    }
+
+    private static void SpecialQuery(String input,DirectoryCorpus corpus, Index index) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IOException {
+        switch (input) {
+            case ":q" -> System.exit(0);
+            case ":vocab" -> Vocab(index);
+        }
+      String[] strings = input.split(" ");
+        if (strings[0].equals(":index")){
+            corpus = IndexFromFile(strings[1]);
+            index = indexCorpus(corpus);
+        }else if (strings[0].equals(":term")){
+            System.out.println(Stem(strings[1]));
+        }
+    }
+    private static DirectoryCorpus IndexFromFile(String string) throws IOException {
+        if (isValidPath(string)){
+                    DirectoryCorpus directoryCorpus = new DirectoryCorpus(Path.of(string));
+            directoryCorpus.registerFileDocumentFactory(".json", JsonFileDocument::loadJsonFileDocument);
+            directoryCorpus.registerFileDocumentFactory(".txt", TextFileDocument::loadTextFileDocument);
+            return directoryCorpus;
+        }
+        return findCorpus();
+    }
+    private static void Vocab(Index index){
+        List<String> terms = index.getVocabulary();
+        for (int i=0;i<1000;i++){
+            System.out.println(terms.get(i));
+        }
+        System.out.println(terms.size());
+    }
+
+    private static String Stem(String term) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        Class stemClass = Class.forName("org.tartarus.snowball.ext." + "englishStemmer");
+        SnowballStemmer stemmer = (SnowballStemmer) stemClass.newInstance();
+        stemmer.setCurrent(term);
+        stemmer.stem();
+        return stemmer.getCurrent();
     }
 
     private static DirectoryCorpus findCorpus() throws IOException {
