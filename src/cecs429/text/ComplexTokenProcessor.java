bcbcdc6e9c3c4dfb218bc5d8c1c5e54c31974e97
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2022-10-02 14:11:26 -0700

    queries

--- src/cecs429/text/ComplexTokenProcessor.java
+++ src/cecs429/text/ComplexTokenProcessor.java
@@ -5,47 +5,21 @@ import org.tartarus.snowball.SnowballStemmer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ComplexTokenProcessor implements TokenProcessor {
     private Pattern removeNonAlpha=Pattern.compile("^[\\W_]|[\\W_]$");
 
-    public static void replaceAll(StringBuilder builder, String from, String to) {
-        int index = builder.indexOf(from);
-        while (index != -1) {
-            builder.replace(index, index + from.length(), to);
-            index += to.length(); // Move to the end of the replacement
-            index = builder.indexOf(from, index);
-        }
-    }
-
-    public static void toLowerCase(StringBuilder builder) {
-        for (int i = 0; i < builder.length(); i++) {
-            if (Character.isUpperCase(builder.charAt(i))) {
-                builder.setCharAt(i,
-                        Character.toLowerCase(builder.charAt(i)));
-            }
-        }
-    }
-
     @Override
     public List<String> processToken(String token) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         List<String> result = new ArrayList<>();
-        StringBuilder stringBuilder = new StringBuilder();
-        Matcher m = removeNonAlpha.matcher(token);
-        while (m.find()){
-            m.appendReplacement(stringBuilder,"");
-        }
-        m.appendTail(stringBuilder);
-        replaceAll(stringBuilder,"\"","");
-        replaceAll(stringBuilder,"'","");
+        token = token.replaceAll("^[\\W_]|[\\W_]$","");
+        token = token.replaceAll("\"'","");
         if (token.contains("-")){
-               String[] tokens = stringBuilder.toString().toLowerCase().split("-");
-               replaceAll(stringBuilder,"-","");
-               toLowerCase(stringBuilder);
+               String[] tokens = token.toLowerCase().split("-");
+               token = token.replaceAll("-","").toLowerCase();
                List<String> strings= new ArrayList<>(Arrays.asList(tokens));
-               strings.add(stringBuilder.toString());
+               strings.add(token);
             Class stemClass = Class.forName("org.tartarus.snowball.ext." + "englishStemmer");
             SnowballStemmer stemmer = (SnowballStemmer) stemClass.newInstance();
 for (String word : strings){
@@ -55,10 +29,10 @@ for (String word : strings){
 }
 
         } else {
-            toLowerCase(stringBuilder);
+            token = token.toLowerCase();
             Class stemClass = Class.forName("org.tartarus.snowball.ext." + "englishStemmer");
             SnowballStemmer stemmer = (SnowballStemmer) stemClass.newInstance();
-            stemmer.setCurrent(stringBuilder.toString());
+            stemmer.setCurrent(token);
             stemmer.stem();
            result.add(stemmer.getCurrent());
         }
