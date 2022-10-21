f06e878953fa367cd5d224a0f7447cee8c9dda7f
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2022-10-21 16:37:00 -0700

    sql

--- src/cecs429/indexing/DiskIndexWriter.java
+++ src/cecs429/indexing/DiskIndexWriter.java
@@ -4,10 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.file.Path;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.List;
 
 public class DiskIndexWriter {
@@ -15,25 +12,36 @@ public class DiskIndexWriter {
         RandomAccessFile randomAccessFile = new RandomAccessFile(absoluteFilePath,"rw");
         List<String> vocabulary = index.getVocabulary();
         Connection connection = DriverManager.getConnection("jdbc:sqlite:terms.sqlite");
-        Statement statement = connection.createStatement();
+        PreparedStatement statement = connection.prepareStatement("insert into terms values(?,?)");
         statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
+        int bytepos = 0;
         for (String term : vocabulary){
             List<Posting> postings = index.getPostings(term);
             // dft
+            int newBytePos = bytepos;
+            newBytePos+=4;
             randomAccessFile.writeInt(postings.size());
             int lastID = 0;
             for (Posting posting : postings){
+                newBytePos+=4;
                 randomAccessFile.writeInt(posting.getDocumentId() - lastID);
                 lastID = posting.getDocumentId();
                 int lastPos = 0;
+                newBytePos+=4;
                 randomAccessFile.writeInt(posting.getTftd());
                 for (int position : posting.getPos()){
+                    newBytePos+=4;
                     randomAccessFile.writeInt(position-lastPos);
                     lastPos=position;
                 }
-                statement.executeUpdate("insert into terms values(term,1)");
             }
+
+            statement.setString(1, term);
+            statement.setInt(2, bytepos);
+            statement.executeUpdate();
+
+            bytepos=newBytePos;
         }
     }
 }
